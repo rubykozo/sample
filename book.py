f90ef3d2031a6015a6b24e9de3ddb496a39428ef
tarou <tarou@example.com>
2025-08-06 13:12:45 +0900

    book.py was coded

---
+++ book.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, title):
+        self._title = title
+
+    
+    def show(self):
+        print('{}'.format(self._title))
+
+
+if __name__ == '__main__':
+    book = Book('Python programming II')
+    book.show()
